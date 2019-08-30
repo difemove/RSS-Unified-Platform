2a0e9f623c672f8d1f3dbc7e028c0335040ed235
Freddy Montañez <C0ndeAstar0th@gmail.com>
2019-08-30 10:11:32 -0500

    Primer ensamble funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AutoTemplateComponent } from './components/auto-template/auto-template.component';
+import { TemplateComponent } from './components/template/template.component';
+import { CustomerTypesComponent } from './components/Ticket-DescriptionAPP/customer-types/customer-types.component';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,8 +14,9 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
-import { CustomerTypesComponent } from './components/Ticket-DescriptionAPP/customer-types/customer-types.component';
+
 import { MatInputModule } from '@angular/material';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { ContractTypesComponent } from './components/Ticket-DescriptionAPP/contract-types/contract-types.component';
 import { ProductsComponent } from './components/Ticket-DescriptionAPP/products/products.component';
@@ -28,6 +33,31 @@ import 'hammerjs';
 import { HeaderComponent } from './components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CopyService } from './services/copyService';
+
+//Angular Material Components
+
+import {MatButtonModule} from '@angular/material';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,18 +67,56 @@ import { CopyService } from './services/copyService';
     WarrantiesComponent,
     AditionalInformationComponent,
     TicketDescriptionComponent,
-    HeaderComponent
+    HeaderComponent,
+    TemplateComponent,
+    AutoTemplateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatInputModule, BrowserAnimationsModule, FormsModule,
-    MatSlideToggleModule, MatRadioModule, MatCardModule,
+    MatInputModule, 
+    BrowserAnimationsModule, 
+    FormsModule,
+    MatSlideToggleModule, 
+    MatRadioModule, 
+    MatCardModule,
   MatCheckboxModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,      
+    MatCheckboxModule,
+    MatButtonModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
     
   ],
   providers: [CopyService],
